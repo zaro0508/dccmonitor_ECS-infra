33e41505d4d514d60c51d724fb27dd4ee97c17ee
bhoff <bruce.hoff@sagebase.org>
2023-05-03 14:50:44 -0700

    deploy dccmonitor for amp-ad

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 
-CONTEXT_ENVS = ["1kd-dev", "pec-dev", "1kd-prod", "pec-prod"]
+CONTEXT_ENVS = ["1kd-dev", "pec-dev", "amp-ad-dev", "1kd-prod", "pec-prod", "amp-ad-prod"]
 TAGS_CONTEXT = "TAGS"
 STACK_NAME_PREFIX_CONTEXT = "STACK_NAME_PREFIX"
